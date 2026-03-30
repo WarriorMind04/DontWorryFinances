8fcd5177c8751ebe329334007043939a33b237c0
WarriorMind04 <88904471+WarriorMind04@users.noreply.github.com>
2026-03-30 11:01:22 +0200

    Initial Commit

---
+++ DWF/DWFApp.swift
@@ -0,0 +1,17 @@
+//
+//  DWFApp.swift
+//  DWF
+//
+//  Created by José Miguel Guerrero Jiménez on 30/03/26.
+//
+
+import SwiftUI
+
+@main
+struct DWFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
